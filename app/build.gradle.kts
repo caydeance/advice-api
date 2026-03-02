ddf7305ca8c5b3bf90309954f5f8dfaa70b284af
MeganS <Meganselden3@gmail.com>
2026-03-02 10:00:34 -0800

    fixed dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.gson)
     implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
