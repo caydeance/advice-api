2de395ba23b95afe6c2b4ffc1799479680cd3beb
caydeance <cadencenee@gmail.com>
2026-03-06 09:13:40 -0800

    rebased

--- app/src/main/java/com/example/adviceapi/AdviceSlipActivity.kt
+++ app/src/main/java/com/example/adviceapi/AdviceSlipActivity.kt
@@ -1,28 +1,6 @@
 package com.example.adviceapi
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.adviceapi.databinding.AdviceSlipBinding
-import com.example.adviceapi.databinding.VentBinding
-
-class AdviceSlipActivity : AppCompatActivity() {
-
-    private lateinit var binding: AdviceSlipBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = AdviceSlipBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.adviceLayout) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-    }
-
+class AdviceSlipActivity {
+    val adviceService = RetrofitHelper.getInstance().create(AdviceService::class.java)
+    val adviceCall = adviceService.getAdvice()
 }
