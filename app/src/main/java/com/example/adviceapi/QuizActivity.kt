d40d9de27dd4c6db10198ad98697e93530ec1f05
MeganS <Meganselden3@gmail.com>
2026-03-06 10:10:09 -0800

    quiz activity

--- app/src/main/java/com/example/adviceapi/QuizActivity.kt
+++ app/src/main/java/com/example/adviceapi/QuizActivity.kt
@@ -2,11 +2,13 @@ package com.example.adviceapi
 
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.adviceapi.databinding.QuizBinding
+import com.google.gson.Gson
 
 
 class QuizActivity : AppCompatActivity() {
@@ -22,6 +24,13 @@ class QuizActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var gson = Gson()
+        val inputStream = resources.openRawResource(R.raw.quiz)
+        val jsonString = inputStream.bufferedReader().use{
+            it.readText()
+        }
+
 
     }
 
