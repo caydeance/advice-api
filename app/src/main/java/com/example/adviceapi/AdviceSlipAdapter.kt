acd15055d696b2af31250ba2a4e59bc4d5cbdda9
caydeance <cadencenee@gmail.com>
2026-03-11 22:03:34 -0700

    commit for AdviceSlipAdapter.kt and advice_item.xml (didn't commit first time)

---
+++ app/src/main/java/com/example/adviceapi/AdviceSlipAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.adviceapi
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.widget.TextViewCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class AdviceSlipAdapter (var adviceSlipList: List<Slip>) :
+    RecyclerView.Adapter<AdviceSlipAdapter.ViewHolder>() {
+    companion object {
+        val EXTRA_ADVICE = "advice"
+    }
+
+    private var adviceSlipCount : Int = 0
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textViewQuote: TextView
+        val textViewId: TextView
+        val layout: ConstraintLayout
+
+        init {
+            textViewQuote = view.findViewById(R.id.textView_adviceSlipItem_quote)
+            textViewId = view.findViewById(R.id.textView_adviceSlipItem_id)
+            layout = view.findViewById(R.id.layout_adviceSlipItem)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.advice_item, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val adviceSlip = adviceSlipList[position]
+
+        viewHolder.textViewId.text = adviceSlip.advice
+        viewHolder.textViewId.text = adviceSlip.id.toString()
+
+
+
+    }
+
+    override fun getItemCount() = adviceSlipList.size
+}
