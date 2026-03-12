cf395a62f216663c6707551b712dc2b23856b69d
MeganS <Meganselden3@gmail.com>
2026-03-12 09:15:52 -0700

    working Quiz activity

--- app/src/main/java/com/example/adviceapi/QuizActivity.kt
+++ app/src/main/java/com/example/adviceapi/QuizActivity.kt
@@ -11,7 +11,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.Group
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.adviceapi.databinding.ActivityMainBinding
 import com.example.adviceapi.databinding.ActivityQuizBinding
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -39,6 +38,13 @@ class QuizActivity : AppCompatActivity() {
     private lateinit var question2 : TextView
 
     private lateinit var choice5: Button
+
+    private lateinit var group3: Group
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var button3: Button
+    private lateinit var group3Q : TextView
+
 
 
 
@@ -68,6 +74,8 @@ class QuizActivity : AppCompatActivity() {
         var quiz = Quiz(questions)
 
         group2.visibility = View.GONE
+        group3.visibility = View.GONE
+
         var choices = quiz.getChoice()
 
         choice1.text = choices[0]
@@ -90,12 +98,18 @@ class QuizActivity : AppCompatActivity() {
                 var choices = quiz.getChoice()
                 if(choices.size == 4){
                     query.text = quiz.getCurrQ()
+                    choicesGroup.visibility = View.VISIBLE
+                    group2.visibility = View.GONE
+                    group3.visibility = View.GONE
                     choice1.text = choices[0]
                     choice2.text = choices[1]
                     choice3.text = choices[2]
                     choice4.text = choices[3]
                 }
                 if(choices.size == 5){
+                    choicesGroup.visibility = View.VISIBLE
+                    group2.visibility = View.GONE
+                    group3.visibility = View.GONE
                     choice5.visibility = View.VISIBLE
                     query.text = quiz.getCurrQ()
                     choice1.text = choices[0]
@@ -107,10 +121,21 @@ class QuizActivity : AppCompatActivity() {
                 if(choices.size == 2){
                     choicesGroup.visibility = View.GONE
                     group2.visibility = View.VISIBLE
+                    group3.visibility = View.GONE
                     question2.text = quiz.getCurrQ()
                     ans1.text = choices[0]
                     ans2.text = choices[1]
                 }
+                if(choices.size == 3){
+                    choicesGroup.visibility = View.GONE
+                    group2.visibility = View.GONE
+                    group3.visibility = View.VISIBLE
+                    group3Q.text = quiz.getCurrQ()
+                    button1.text = choices[0]
+                    button2.text = choices[1]
+                    button3.text = choices[2]
+                }
+
 
             }
         }
@@ -148,16 +173,35 @@ class QuizActivity : AppCompatActivity() {
 
         ans1.setOnClickListener {
             quiz.check(ans1.text.toString())
-            group2.visibility = View.GONE
-            choicesGroup.visibility = View.VISIBLE
+//            group2.visibility = View.GONE
+//            choicesGroup.visibility = View.VISIBLE
             getNextQuestion()
 
         }
 
         ans2.setOnClickListener {
             quiz.check(ans2.text.toString())
-            group2.visibility = View.GONE
-            choicesGroup.visibility = View.VISIBLE
+//            group2.visibility = View.GONE
+//            choicesGroup.visibility = View.VISIBLE
+            getNextQuestion()
+
+
+        }
+
+        button1.setOnClickListener {
+            quiz.check(button1.text.toString())
+            getNextQuestion()
+
+
+        }
+        button2.setOnClickListener {
+            quiz.check(button2.text.toString())
+            getNextQuestion()
+
+
+        }
+        button3.setOnClickListener {
+            quiz.check(button3.text.toString())
             getNextQuestion()
 
 
@@ -172,15 +216,23 @@ class QuizActivity : AppCompatActivity() {
         choice3 = findViewById<Button>(R.id.choice3)
         choice4 = findViewById<Button>(R.id.choice4)
         query = findViewById<TextView>(R.id.question)
+        choicesGroup = findViewById<Group>(R.id.fourQuestionGroup)
 
         choice5 = findViewById(R.id.choice5)
 
 
-        choicesGroup = findViewById<Group>(R.id.fourQuestionGroup)
+
         group2 = findViewById(R.id.twoQuestionGroup)
         ans1 = findViewById(R.id.ans1)
         ans2 = findViewById(R.id.ans2)
         question2 = findViewById(R.id.group2Q)
+
+        group3 = findViewById(R.id.group3)
+        group3Q = findViewById(R.id.group3Q)
+        button1 = findViewById(R.id.button1)
+        button2 = findViewById(R.id.button2)
+        button3 = findViewById(R.id.button3)
+
 
 
     }
